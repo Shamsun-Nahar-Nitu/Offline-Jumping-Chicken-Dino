823c1b7493a29db84d6ccbce129e8bd8186ed63c
Shamsun-Nahar-Nitu <124525218+Shamsun-Nahar-Nitu@users.noreply.github.com>
2026-01-27 23:34:50 +0600

    chicken

--- chicken.py
+++ chicken.py
@@ -1,14 +1,12 @@
-#Chicken Character Module
+
+# Chicken Character Module
 
 
 import pygame
 from game_config import *
 
 
-class Chicken:
-    # The main player character
-
-    
+class Chicken:   
     def __init__(self):
         # Position
         self.x = CHICKEN_X
@@ -28,7 +26,6 @@ class Chicken:
         self.create_chicken_sprite()
     
     def create_chicken_sprite(self):
-        
         # Create two frames for running animation
         self.sprites = []
         
@@ -43,13 +40,6 @@ class Chicken:
         self.sprites.append(sprite2)
     
     def draw_chicken(self, surface, leg_forward):
-        """
-        Draw a pixel art chicken on the given surface
-        
-        Args:
-            surface: The pygame surface to draw on
-            leg_forward: Boolean to determine which leg is forward (for animation)
-        """
         # Colors for the chicken
         body_color = (255, 200, 100)  # Orange-yellow body
         beak_color = (255, 165, 0)    # Orange beak
@@ -57,60 +47,55 @@ class Chicken:
         comb_color = (255, 0, 0)      # Red comb
         leg_color = (255, 165, 0)     # Orange legs
         
-        # Draw body (main oval shape)
+        # Draw body 
         # Body is positioned in the middle-upper part
-        pygame.draw.ellipse(surface, body_color, (8, 20, 30, 25))
+        pygame.draw.ellipse(surface, body_color, (6, 20, 30, 25))
         
-        # Draw head (smaller circle)
-        pygame.draw.circle(surface, body_color, (12, 15), 8)
+        # Draw head
+        pygame.draw.circle(surface, body_color, (32, 15), 8)
         
-        # Draw comb (red thing on top of head)
-        comb_points = [(10, 8), (12, 4), (14, 8)]
+        # Draw comb
+        comb_points = [(30, 8), (32, 4), (34, 8)]
         pygame.draw.polygon(surface, comb_color, comb_points)
         
-        # Draw eye (small black dot)
-        pygame.draw.circle(surface, eye_color, (10, 14), 2)
+        # Draw eye 
+        pygame.draw.circle(surface, eye_color, (34, 14), 2)
         
-        # Draw beak (small triangle)
-        beak_points = [(5, 16), (2, 15), (5, 14)]
+        # Draw beak 
+        beak_points = [(39, 16), (42, 15), (39, 14)]
         pygame.draw.polygon(surface, beak_color, beak_points)
         
-        # Draw tail feathers (pointed back)
-        pygame.draw.polygon(surface, body_color, [(35, 28), (42, 25), (38, 32)])
-        pygame.draw.polygon(surface, body_color, [(35, 32), (42, 30), (38, 36)])
+        # Draw tail 
+        pygame.draw.polygon(surface, body_color, [(9, 28), (2, 25), (6, 32)])
+        pygame.draw.polygon(surface, body_color, [(9, 32), (2, 30), (6, 36)])
         
-        # Draw legs - position changes based on animation
+        # Draw legs 
         if leg_forward:
             # Left leg forward
-            pygame.draw.rect(surface, leg_color, (15, 45, 3, 12))
+            pygame.draw.rect(surface, leg_color, (18, 45, 3, 12))
             # Right leg back
-            pygame.draw.rect(surface, leg_color, (25, 45, 3, 12))
+            pygame.draw.rect(surface, leg_color, (28, 45, 3, 12))
         else:
             # Right leg forward
-            pygame.draw.rect(surface, leg_color, (25, 45, 3, 12))
+            pygame.draw.rect(surface, leg_color, (28, 45, 3, 12))
             # Left leg back
-            pygame.draw.rect(surface, leg_color, (15, 45, 3, 12))
+            pygame.draw.rect(surface, leg_color, (18, 45, 3, 12))
         
-        # Draw feet (small rectangles at bottom of legs)
+        # Draw feet 
         if leg_forward:
-            pygame.draw.rect(surface, leg_color, (13, 57, 6, 2))
-            pygame.draw.rect(surface, leg_color, (23, 57, 6, 2))
+            pygame.draw.rect(surface, leg_color, (16, 57, 6, 2))
+            pygame.draw.rect(surface, leg_color, (26, 57, 6, 2))
         else:
-            pygame.draw.rect(surface, leg_color, (23, 57, 6, 2))
-            pygame.draw.rect(surface, leg_color, (13, 57, 6, 2))
+            pygame.draw.rect(surface, leg_color, (26, 57, 6, 2))
+            pygame.draw.rect(surface, leg_color, (16, 57, 6, 2))
     
     def jump(self):
-        """
-        Make the chicken jump if it's on the ground
-        """
         if not self.is_jumping:
             self.velocity_y = JUMP_STRENGTH
             self.is_jumping = True
     
     def update(self):
-        """
-        Update chicken position and physics each frame
-        """
+
         # Apply gravity
         self.velocity_y += GRAVITY
         self.y += self.velocity_y
@@ -121,7 +106,7 @@ class Chicken:
             self.velocity_y = 0
             self.is_jumping = False
         
-        # Update running animation (only when on ground)
+        # Update running animation 
         if not self.is_jumping:
             self.animation_counter += 1
             if self.animation_counter >= 10:  # Change frame every 10 updates
@@ -129,23 +114,11 @@ class Chicken:
                 self.animation_frame = (self.animation_frame + 1) % 2
     
     def draw(self, screen):
-        """
-        Draw the chicken on the screen
-        
-        Args:
-            screen: The pygame screen surface to draw on
-        """
         # Use the current animation frame
         current_sprite = self.sprites[self.animation_frame]
         screen.blit(current_sprite, (self.x, self.y))
     
     def get_rect(self):
-        """
-        Get the chicken's collision rectangle
-        Returns a slightly smaller rect for better collision feel
-        
-        Returns:
-            pygame.Rect: The collision box
-        """
-        # Make hitbox slightly smaller for fairer collision
+
+        # Make hitbox smaller
         return pygame.Rect(self.x + 5, self.y + 5, self.width - 10, self.height - 10)
